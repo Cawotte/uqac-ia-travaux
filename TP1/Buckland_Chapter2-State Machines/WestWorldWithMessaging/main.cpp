c0a34b415a965eaaf64958c72b45a8020120dc0a
Ensuque <khamaz.ee@gmail.com>
2020-05-20 11:23:05 -0400

    Ask initial starting state

--- TP1/Buckland_Chapter2-State Machines/WestWorldWithMessaging/main.cpp
+++ TP1/Buckland_Chapter2-State Machines/WestWorldWithMessaging/main.cpp
@@ -16,9 +16,13 @@
 std::ofstream os;
 
 // ----- VARIABLES  -------
-bool		const USE_MULTITHREADING	= true;
+bool		const USE_MULTITHREADING	= false;
+bool        const ASK_STARTING_STATE    = true;
 int			const NB_ITERATIONS			= 30;
 int			const UPDATE_SLEEP_TIME		= 800;
+
+
+void askInitialState(Miner* bob, MinersWife* elsa, Alcoholic* alcoholic);
 
 int main()
 {
@@ -41,6 +45,10 @@ int main()
 	//create his arch-nemesis
 	std::unique_ptr<Alcoholic> alcoholic = std::make_unique<Alcoholic>(ent_Alcoholic); 
 
+    if (ASK_STARTING_STATE)
+    {
+        askInitialState(bob.get(), elsa.get(), alcoholic.get());
+    }
 
 	//register them with the entity manager
 	EntityMgr->RegisterEntity(bob.get());
@@ -89,6 +97,76 @@ int main()
 
 
 	return 0;
+}
+
+void askInitialState(Miner* bob, MinersWife* elsa, Alcoholic* alcoholic)
+{
+    int bobState = 0;
+    printf("\nPlease choose Bob's initial State : \n1. GoHomeAndSleepTilRested (default)   \n2. EnterMineAndDigForNugget    \n3. VisitBankAndDepositGold   \n4. QuenchThirst \n5. EatStew \n\n Choice : ");
+    scanf_s("%i", &bobState);
+
+    int elsaState = 0;
+
+    printf("\nPlease choose Elsa's initial State : \n1. DoHouseWork (default)   \n2. VisitBathroom   \n3. CookStew  \n\n Choice : ");
+    scanf_s("%i", &elsaState);
+
+    int alcoholicState = 0;
+    printf("\nPlease choose Alcholic's initial State : \n1. DrinkBeer (default)   \n2. Drunk \n\n Choice : ");
+    scanf_s("%i", &alcoholicState);
+
+
+
+    switch (bobState)
+    {
+    case 1:
+        bob->GetFSM()->SetCurrentState(GoHomeAndSleepTilRested::Instance());
+        break;
+
+    case 2:
+        bob->GetFSM()->SetCurrentState(EnterMineAndDigForNugget::Instance());
+        break;
+
+    case 3:
+        bob->GetFSM()->SetCurrentState(VisitBankAndDepositGold::Instance());
+        break;
+    case 4:
+        bob->GetFSM()->SetCurrentState(QuenchThirst::Instance());
+        break;
+    case 5:
+        bob->GetFSM()->SetCurrentState(EatStew::Instance());
+        break;
+    default:
+        bob->GetFSM()->SetCurrentState(GoHomeAndSleepTilRested::Instance());
+        break;
+    }
+
+    switch (elsaState)
+    {
+    case 1:
+        elsa->GetFSM()->SetCurrentState(DoHouseWork::Instance());
+        break;
+    case 2:
+        elsa->GetFSM()->SetCurrentState(VisitBathroom::Instance());
+
+    default:
+        elsa->GetFSM()->SetCurrentState(CookStew::Instance());
+        break;
+    }
+
+    switch (alcoholicState)
+    {
+    case 1:
+        alcoholic->GetFSM()->SetCurrentState(DrinkBeer::Instance());
+        break;
+    case 2:
+        alcoholic->GetFSM()->SetCurrentState(Drunk::Instance());
+        break;
+    default:
+        alcoholic->GetFSM()->SetCurrentState(DrinkBeer::Instance());
+        break;
+    }
+
+
 }
 
 
